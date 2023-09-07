93812568b030deda4d7cfef251ac2966c12040ed
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-07 14:51:38 -0700

    4-pusts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main -A program that orints a line with pust function
-* Return: 0 (Success)
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-pust("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle"
+);
+	return (0);
 }
