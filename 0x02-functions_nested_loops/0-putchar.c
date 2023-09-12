15b562e585152460eab4d784340f673c85922d9c
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-12 06:01:27 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,7 @@
 * main - Entry point
 * Return:0
 */
-
-int main(void)
+int _putchar(char c)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+return (write(1, &c, 1));
 }
