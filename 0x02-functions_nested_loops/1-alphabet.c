a5869c21547c061c70115c47bdfdac49abdeeed3
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-12 06:49:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+*print_alphabet - make the alphabet
+*
+*iReturn:void
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
