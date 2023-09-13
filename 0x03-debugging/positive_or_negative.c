bac1c3b96d40e0ebdba37bbd5f7916080778f016
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-13 05:15:32 -0700

    0x03-debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i);
+
+{
+
+if (i > 0)
+printf("%d is postive\n", i);
+eles if(i > 0)
+printf("%d is postive\n", i);
+eles 
+printf("%d is zero\n", i);
+}
