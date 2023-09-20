14931a0d5e49e30785a5cbef1fc43a46d5b54520
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-20 11:32:41 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -8,11 +8,10 @@
       */
 int main(void)
 {
-	    char str[] = "Look up!\n";
-	        char *ptr;
-
-		    ptr = string_toupper(str);
-		        printf("%s", ptr);
-			    printf("%s", str);
-			        return (0);
+char str[] = "Look up!\n";
+char *ptr;
+ptr = string_toupper(str);
+printf("%s", ptr);
+printf("%s", str);
+return (0);
 }
