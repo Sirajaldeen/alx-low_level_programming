89d9589c4762f134c07c273e389880db4f0ae821
sirajaldeen <sergio.ust.it@gmail.com>
2023-09-28 06:07:57 -0700

    all

--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -8,15 +7,14 @@
  */
 int main(void)
 {
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
+int r;
+r = is_palindrome("level");
+printf("%d\n", r);
+r = is_palindrome("redder");
+printf("%d\n", r);
+r = is_palindrome("test");
+printf("%d\n", r);
+r = is_palindrome("step on no pets");
+printf("%d\n", r);
+return (0);
 }
