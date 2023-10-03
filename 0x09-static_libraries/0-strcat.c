5641c8c03793efc63d850eee4b36563083abf744
sirajaldeen <sergio.ust.it@gmail.com>
2023-10-02 23:01:25 -0700

    libmy

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
    * *_strcat - concatenates two strings
     * @dest: pointer destination
      * @src: pointer source
       * Return: void
       */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 int a = -1, i;
 for (i = 0; dest[i] != '\0'; i++)
@@ -17,5 +16,9 @@ a++;
 dest[i] = src[a];
 i++;
 }while (src[a] != '\0');
-return (dest);
+return dest;
+}
+int main()
+{
+return 0;
 }
